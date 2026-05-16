425cfe546934db97314217c913efb985b1467f0b
Bill Guedel <wsguede@users.noreply.github.com>
2026-05-15 23:13:25 -0500

    Refactor install.sh to streamline setup process
    Updated installation script to include additional tools and configurations.

--- install.sh
+++ install.sh
@@ -3,31 +3,32 @@
 #
 # This is what should be run on a fresh install. This should be run as sudo
 
-
-# Install zsh first
-apt update
-apt install -y zsh git curl
-apt autoremove -y
-apt clean
-
-
-# Change the skel for a new user
-rm -f /etc/skel/.profile /etc/skel/.bash*
-curl -fsSLo /etc/skel/.zshrc "https://raw.githubusercontent.com/wsguede/ohmyzsh/master/.zshrc"
-mkdir -p /etc/skel/.oh-my-zsh/plugins /etc/skel/.oh-my-zsh/themes
-chmod -R 755 /etc/skel
-chown root:root /etc/skel
-# change root to have zsh file
-rm -f /root/.profile /root/.bash*
-yes | cp -R /etc/skel/ /root/
+sudo apt update && sudo apt install \
+  zsh \
+  git \
+  curl \
+  direnv \
+  xclip -y
 
 
-# Change adduser to use zsh by default. Also change root user to use zsh
-useradd -D --shell /bin/zsh
-useradd -s /bin/zsh root
+# install fnm; prevent the configuration. 
+curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
-export ZSH=/etc/oh-my-zsh
-# install oh-my-zsh globally
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# add syntax highlighting to the global config. This might be a bad idea. Will probably never get updated, might get overwritten on upgrade
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH}/plugins/zsh-syntax-highlighting
+# install node with fast node manager; and configure npm
+fnm install --lts
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+
+# install zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# install omz
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+
+# intall fast-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
